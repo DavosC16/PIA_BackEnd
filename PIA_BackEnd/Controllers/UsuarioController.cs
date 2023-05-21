ef96738875ee4358109c615a3a18dc7e26a5528d
Aylin <aylindemetci@gmail.com>
2023-05-20 23:07:17 -0600

    post y get usuario

--- PIA_BackEnd/Controllers/UsuarioController.cs
+++ PIA_BackEnd/Controllers/UsuarioController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PIA_BackEnd.DTOs;
+using PIA_BackEnd.Entidades;
 
 namespace PIA_BackEnd.Controllers
 {
@@ -29,6 +30,28 @@ namespace PIA_BackEnd.Controllers
         {
             var alumnos = await dbContext.Usuario.ToListAsync();
             return mapper.Map<List<GetUsuarioDTO>>(alumnos);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] UsuarioDTO usuarioDto)
+        {
+            //Ejemplo para validar desde el controlador con la BD con ayuda del dbContext
+
+            var existeUsuarioMismoNombre = await dbContext.Usuario.AnyAsync(x => x.Nombre == usuarioDto.Nombre);
+
+            if (existeUsuarioMismoNombre)
+            {
+                return BadRequest($"Ya existe un usuario con el nombre {usuarioDto.Nombre}");
+            }
+
+            var usuario = mapper.Map<Usuario>(usuarioDto);
+
+            dbContext.Add(usuario);
+            await dbContext.SaveChangesAsync();
+
+            var usuarioDTO = mapper.Map<GetUsuarioDTO>(usuario);
+
+            return CreatedAtRoute("obteneralumno", new { id = usuario.Id }, usuarioDTO);
         }
     }
 }
